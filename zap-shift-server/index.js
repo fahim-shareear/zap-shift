f3a0f93e6f4b7b3b449c32e8662246132fa902b1
Fahim <fahimshareear@gmail.com>
2026-02-01 21:43:08 +0600

    adding server connection to the mongodb

---
+++ zap-shift-server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res)=>{
+    res.send("Server is locally up and running");
+});
+
+async function run(){
+    try{
+        await client.connect();
+
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged the server, Server Connection is successfull to MongoDB!");
+    }
+    finally{
+
+    }
+};
+
+run().catch(console.dir);
+
+
+app.listen(port, ()=>{
+    console.log(`local server is running on ${port}`)
+});
