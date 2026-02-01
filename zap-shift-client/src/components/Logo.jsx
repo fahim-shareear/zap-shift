50ce05b012e5ec6edb7ba9cfd1135a819fa46a62
Fahim <fahimshareear@gmail.com>
2026-02-01 23:23:14 +0600

    removing env file & designing navbar

--- zap-shift-client/src/components/Logo.jsx
+++ zap-shift-client/src/components/Logo.jsx
@@ -3,9 +3,9 @@ import logo from "../assets/logo.png";
 
 const Logo = () => {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-end justify-center">
             <img src={logo} alt="logo" />
-            <h1 className="font-bold text-xl mt-4 text-[#303030] ml-0">ZapShift</h1>
+            <h1 className="font-bold text-3xl  text-[#303030] -ms-2.5">ZapShift</h1>
         </div>
     );
 };
--- zap-shift-client/src/pages/shared/navbar/Navbar.jsx
+++ zap-shift-client/src/pages/shared/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from '../../../components/Logo';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar bg-base-100 shadow-sm rounded-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
