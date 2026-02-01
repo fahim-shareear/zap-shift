3e366895d7860516a9e375dc2f34b35ff9ad7239
Fahim <fahimshareear@gmail.com>
2026-02-01 22:15:43 +0600

    fixing various security issues

--- zap-shift-server/index.js
+++ zap-shift-server/index.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
